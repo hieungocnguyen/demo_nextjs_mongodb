a315e5374b973936bfd8de081709702c80420a0c
nguyenngochieu <ngnohieu@gmail.com>
2022-11-23 20:28:57 +0700

    test

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,39 +1,39 @@
-import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "../../styles/Post.module.css";
-const Post = ({ post }) => {
-   const router = useRouter();
+// import axios from "axios";
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+// import styles from "../../styles/Post.module.css";
+// const Post = ({ post }) => {
+//    const router = useRouter();
 
-   const deletePost = async () => {
-      const postID = router.query.id;
-      try {
-         const deleted = await axios.delete(`/api/posts/${postID}`);
+//    const deletePost = async () => {
+//       const postID = router.query.id;
+//       try {
+//          const deleted = await axios.delete(`/api/posts/${postID}`);
 
-         router.push("/");
-      } catch (error) {
-         console.log(error);
-      }
-   };
+//          router.push("/");
+//       } catch (error) {
+//          console.log(error);
+//       }
+//    };
 
-   return (
-      <div className={styles.container}>
-         <Link href={`/`}>
-            <div>Back to home</div>
-         </Link>
-         <div className={styles.post}>
-            <div className={styles.title}>{post.title}</div>
-            <div>{post.content}</div>
-         </div>
-         <button onClick={deletePost}>Delete this post</button>
-      </div>
-   );
-};
+//    return (
+//       <div className={styles.container}>
+//          <Link href={`/`}>
+//             <div>Back to home</div>
+//          </Link>
+//          <div className={styles.post}>
+//             <div className={styles.title}>{post.title}</div>
+//             <div>{post.content}</div>
+//          </div>
+//          <button onClick={deletePost}>Delete this post</button>
+//       </div>
+//    );
+// };
 
-Post.getInitialProps = async ({ query: { id } }) => {
-   const res = await axios.get(`http://localhost:3000/api/posts/${id}`);
+// Post.getInitialProps = async ({ query: { id } }) => {
+//    const res = await axios.get(`http://localhost:3000/api/posts/${id}`);
 
-   return { post: res.data.data };
-};
+//    return { post: res.data.data };
+// };
 
-export default Post;
+// export default Post;
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
-require("dotenv").config();
 const connection = {};
 
 async function dbConnect() {
    if (connection.isConnected) {
       return;
    }
-
    const db = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,

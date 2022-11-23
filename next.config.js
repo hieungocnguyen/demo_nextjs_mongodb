3244991cbc3b002ffc5cd9e4ee94005a60bd5fcb
nguyenngochieu <ngnohieu@gmail.com>
2022-11-23 11:24:43 +0700

    add env file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
-   env: {
-      MONGO_URI:
-         "mongodb+srv://ngnohieu:Ngochieu2001@cluster0.gb71xpy.mongodb.net/?retryWrites=true&w=majority",
-   },
 };
 
 module.exports = nextConfig;
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+require("dotenv").config();
 const connection = {};
 
 async function dbConnect() {

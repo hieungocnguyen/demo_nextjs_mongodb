2562a7c7e9811f1552623daf3d8d338b06d354de
nguyenngochieu <ngnohieu@gmail.com>
2022-11-23 20:35:06 +0700

    test 1

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -5,10 +5,13 @@ async function dbConnect() {
    if (connection.isConnected) {
       return;
    }
-   const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-   });
+   const db = await mongoose.connect(
+      "mongodb+srv://ngnohieu:Ngochieu2001@cluster0.gb71xpy.mongodb.net/?retryWrites=true&w=majority",
+      {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+      }
+   );
 
    connection.isConnected = db.connections[0].readyState;
 }
